b4f5c4a359f761847110931b5a4123049d8da368
6564747347456845 <webkonditer@yandex.ru>
2022-06-15 22:54:03 +0700

    Initial commit

---
+++ src/test/java/com/example/newspringproject/NewspringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
